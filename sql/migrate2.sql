070919e9053ec844d8339a3bfeb195eaa4a4031d
aram.mine <aram.mine@gmail.com>
2016-05-12 16:51:07 +0000

    前夜祭対応 入力フォーム

---
+++ sql/migrate2.sql
@@ -0,0 +1,3 @@
+alter table entry add column before_child tinyint after child;
+alter table entry add column before_adult tinyint after child;
+alter table entry add column before_party tinyint after child;
